ca2080b0f6fdaee20ea7d85be2c8a2fd60d1e521
Choo Wei Xiang <chooweixiang98@gmail.com>
2022-04-03 16:25:18 +0800

    Delete ERC721.sol from contracts folder

--- contracts/ERC721.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import "node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract MyNFT is ERC721Full, ERC721Mintable {
-    constructor() public ERC721Full("MyNFT", "MNFT") {}
-}
